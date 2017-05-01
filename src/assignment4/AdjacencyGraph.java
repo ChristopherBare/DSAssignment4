b23182f9191633e084a0d49c0fee49c80a11abb3
christopher1 <christopher1@christophersmbp.lan>
2017-04-30 23:16:55 -0400

    Edits made on Sunday.

--- src/assignment4/AdjacencyGraph.java
+++ src/assignment4/AdjacencyGraph.java
@@ -1,35 +1,58 @@
 package assignment4;
 
 import DataStructures.ElementNotFoundException;
+import java.util.ArrayList;
 
 /**
- *
- * @author christopher1
+ * @version 1.0
+ * @author cbare3
  */
-class AdjacencyGraph extends Graph {
+class AdjacencyGraph<T> extends Graph {
+
+    private ArrayList<String> nodes = new ArrayList<>();
+    private int[][] Matrix;
+    private int size = 10;
 
     @Override
     void createAdjacencyMatrix() {
-        
+       
     }
 
     @Override
     void addNode(String nodeName) {
-        }
+        //what here?
+        //did it anyway lol
+        //I gave you control of typing not presenting lol'
+        //what goes here....
+        nodes.add(nodeName);
+        
+    
+    }
 
     @Override
-    void addEdge(String fromNode, String toNode, int weight) throws ElementNotFoundException, IllegalStateForMatrixException {
-     }
+    void addEdge(String fromNode, String toNode, int weight){
+        if(Matrix==null)
+        {
+            size = nodes.size();
+        createAdjacencyMatrix();
+    }
 
+    int startIndex = nodes.indexOf(toNode);
+    int endIndex = nodes.indexOf(fromNode);
+    Matrix[startIndex][endIndex] = weight ;
+
+}
+    @Override
+    int getNumberOfEdges() throws IllegalStateForMatrixException {
+        return 0;
+    }
+    
     @Override
     int getNumberOfNodes() {
         return 0;
     }
 
-    @Override
-    int getNumberOfEdges() throws IllegalStateForMatrixException {
-        return 0;
-    }
+    
 
     @Override
     String getHighestDegreeNode() throws ElementNotFoundException, IllegalStateForMatrixException {
@@ -37,12 +60,14 @@ class AdjacencyGraph extends Graph {
     }
 
     @Override
-    int costOfEdgeBetween(String fromNode, String toNode) throws ElementNotFoundException, IllegalStateForMatrixException {
+    int costOfEdgeBetween(String fromNode, String toNode) 
+            throws ElementNotFoundException, IllegalStateForMatrixException {
         return 0;
     }
 
     @Override
-    boolean hasPathBetween(String fromName, String toName) throws ElementNotFoundException, IllegalStateForMatrixException {
+    boolean hasPathBetween(String fromName, String toName) 
+            throws ElementNotFoundException, IllegalStateForMatrixException {
         return false;
     }
 
